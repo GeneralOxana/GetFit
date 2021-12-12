3656d9f996341f7709df22ecbd49f85224bd4b63
GeneralOxana <okcanochka@ya.ru>
2021-12-13 00:14:36 +0400

    ver 1.4.1

--- app/src/main/java/com/example/kotlin10/MainActivity.kt
+++ app/src/main/java/com/example/kotlin10/MainActivity.kt
@@ -1,85 +1,112 @@
 package com.example.kotlin10
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 import kotlin.random.Random
 
-class MainActivity : AppCompatActivity()
-{
+class MainActivity : AppCompatActivity() {
     var counterExe = 0
     var counterSet = 0
+    val calendar: Calendar = Calendar.getInstance()
+    val day: Int = calendar.get(Calendar.DAY_OF_WEEK)
 
 
-    override fun onCreate(first: Bundle?)
-    {
+    override fun onCreate(first: Bundle?) {
         super.onCreate(first)
         setContentView(R.layout.activity_main)
-    }
 
-    fun onClickUpper(view: View)
-    {
-        val myUpper = resources.getStringArray(R.array.myUpper)
-        val arraySize = myUpper.size
-        val rand = Random.nextInt(arraySize)
-        val name = myUpper[rand]
-        var tvMyExe = findViewById<TextView>(R.id.tv_exe)
-        tvMyExe.text = name
+        when (day) {
 
-    }
-
-    fun onClickLower(view: View)
-    {
-        val myLower = resources.getStringArray(R.array.myLower)
-        val arraySize = myLower.size
-        val rand = Random.nextInt(arraySize)
-        //for (i in myLower.indices){
-        val name = myLower[rand]
-        var tvMyExe = findViewById<TextView>(R.id.tv_exe)
-        tvMyExe.text = name
-        //}
-
+            2 -> {val tvDay = findViewById<TextView>(R.id.tv_day)
+                tvDay.text = "Понедельник"
+                onListExe(counterExe, day)}
+            4 -> {val tvDay = findViewById<TextView>(R.id.tv_day)
+                tvDay.text = "Среда"
+                onListExe(counterExe, day)}
+            6 -> {val tvDay = findViewById<TextView>(R.id.tv_day)
+                tvDay.text = "Пятница"
+                onListExe(counterExe, day)}
+            1 -> {val tvDay = findViewById<TextView>(R.id.tv_day)
+                tvDay.text = "Воскресенье - выходной день"}
+            7 -> {val tvDay = findViewById<TextView>(R.id.tv_day)
+                tvDay.text = "Суббота - выходной день"}
+            3 -> {val tvDay = findViewById<TextView>(R.id.tv_day)
+                tvDay.text = "Вторник - выходной день"}
+            5 -> {val tvDay = findViewById<TextView>(R.id.tv_day)
+                tvDay.text = "Четверг - выходной день"}
+        }
     }
 
 
-    fun onClickSet(view: View)
-    {
+    fun onListExe(i: Int, day1: Int) {
+        when {
+            day1 == 2 -> {
+                val myList = resources.getStringArray(R.array.myMonday)
+                val arraySize = myList.size
+                var name = myList[i]
+                var tvMyExe = findViewById<TextView>(R.id.tv_exe)
+                tvMyExe.text = name
+            }
+            day1 == 4 -> {
+                val myList = resources.getStringArray(R.array.myWednesday)
+                val arraySize = myList.size
+                var name = myList[i]
+                var tvMyExe = findViewById<TextView>(R.id.tv_exe)
+                tvMyExe.text = name
+            }
+            day1 == 6 -> {
+                val myList = resources.getStringArray(R.array.myFriday)
+                val arraySize = myList.size
+                var name = myList[i]
+                var tvMyExe = findViewById<TextView>(R.id.tv_exe)
+                tvMyExe.text = name
+            }
+        }
+    }
+
+
+
+
+
+    fun onClickSet(view: View) {
         var countSet = findViewById<TextView>(R.id.set)
         counterSet++
         countSet.text = counterSet.toString()
     }
 
-    fun onClickExe(view: View)
-    {
-        var countExe = findViewById<TextView>(R.id.exe)
-        counterExe++
-        counterSet = 0
-        countExe.text = counterExe.toString()
+    fun onClickExe(view: View) {
+        when {
+            counterExe in 0..4 -> {
+                var countExe = findViewById<TextView>(R.id.exe)
+                counterExe++
+                counterSet = 0
+                countExe.text = counterExe.toString()
 
-        var countSet = findViewById<TextView>(R.id.set)
-        countSet.text = counterSet.toString()
+                var countSet = findViewById<TextView>(R.id.set)
+                countSet.text = counterSet.toString()
 
-
+                onListExe(counterExe, day)
+            }
+            counterExe > 4 -> {
+                var tvQuote = findViewById<TextView>(R.id.tv_quote)
+                tvQuote.text = "Пора домой!!!  "
+            }
+        }
     }
 
-    fun onClickEnd(view: View)
-    {
+    /*fun onClickEnd(view: View) {
         val myQuote = resources.getStringArray(R.array.myQuote)
         val arraySize = myQuote.size
         val rand = Random.nextInt(arraySize)
         val name = myQuote[rand]
         var tvQuote = findViewById<TextView>(R.id.tv_quote)
-        tvQuote.text = name
-    }
+        tvQuote.text = "Пора домой!!!  " + name
+    }*/
 
     fun onClickClose(view: View) {
         super.onDestroy()
     }
-
 }
-
-
-
-
-
